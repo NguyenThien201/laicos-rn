e9bad71996f8e522847e21ba7ebc7a70b798e9ba
Dang Khoa Nguyen <nguyendangkhoa200400@gmail.com>
2021-07-15 00:27:30 +0700

    Thanh điều hướng

--- Laicos/src/Navigators/Main.tsx
+++ Laicos/src/Navigators/Main.tsx
@@ -1,15 +1,75 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { IndexExampleContainer, IndexStartupContainer } from '@/Containers'
+import { Image } from 'react-native'
 
 const Tab = createBottomTabNavigator()
 
 // @refresh reset
 const MainNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={IndexExampleContainer} />
-      <Tab.Screen name="Startup" component={IndexStartupContainer} />
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        activeTintColor: '#3CD3AD',
+        style: {
+          backgroundColor: '#212230',
+          height: 60,
+          borderRadius: 10,
+          margin: 5,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Trang chủ"
+        component={IndexExampleContainer}
+        options={{
+          tabBarLabel: 'Trang chủ',
+          tabBarIcon: ({}) => (
+            <Image source={require('@/Assets/Images/home.png')} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Thống kê"
+        component={IndexStartupContainer}
+        options={{
+          tabBarLabel: 'Thống kê',
+          tabBarIcon: ({}) => (
+            <Image source={require('@/Assets/Images/statistic.png')} />
+          ),
+        }}
+      />
+        <Tab.Screen
+        name="Thêm"
+        component={IndexExampleContainer}
+        options={{
+          tabBarLabel: 'Thêm Giao Dịch',
+          tabBarIcon: ({}) => (
+            <Image source={require('@/Assets/Images/add.png')} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Kế hoạch"
+        component={IndexExampleContainer}
+        options={{
+          tabBarLabel: 'Kế hoạch',
+          tabBarIcon: ({}) => (
+            <Image source={require('@/Assets/Images/plan.png')} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cá nhân"
+        component={IndexStartupContainer}
+        options={{
+          tabBarLabel: 'Trang chủ',
+          tabBarIcon: ({}) => (
+            <Image source={require('@/Assets/Images/account.png')} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   )
 }
