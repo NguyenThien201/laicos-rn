eacc3067a13eb4e66e4efa4cbeafa1b89bb1c21e
Le Van Thuan My <51257500+levanthuanmy@users.noreply.github.com>
2021-07-19 00:07:54 +0700

    update tabview

--- Laicos/src/Screens/Statistic.tsx
+++ Laicos/src/Screens/Statistic.tsx
@@ -1,34 +1,46 @@
-import React from "react"
-import { Dimensions, StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import { Dimensions, Text, View } from "react-native"
 import { LineChart } from "react-native-chart-kit"
+import { SceneMap, TabBar, TabView } from "react-native-tab-view"
 import { globalStyles } from "../styles/theme.style"
-const HomeScreen = () => {
+const Statistic = () => {
   const chartConfig = {
-    backgroundGradientFrom: "#1E2923",
-    backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: "#08130D",
-    backgroundGradientToOpacity: 0.5,
-    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    backgroundGradientFrom: "#151321",
+    backgroundGradientTo: "#151321",
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
     strokeWidth: 2, // optional, default 3
     barPercentage: 0.5,
-    useShadowColorFromDataset: false, // optional
   }
   const data = {
     labels: ["T3", "T4", "T5", "T6", "T7"],
     datasets: [
       {
-        data: [20, 45, 28, 80, 99, 43],
-        color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
-        strokeWidth: 2, // optional
+        data: [0, 1, 1, 1, 0],
+        color: () => `rgba(255, 255, 255, 1)`, // optional
+        strokeWidth: 3, // optional
       },
       {
-        data: [20, 1, 2, 8, 29, 43],
-        color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
-        strokeWidth: 2, // optional
+        data: [10, 10, 10, 15, 15],
+        color: () => `rgba(60, 211, 173, 1)`, // optional
+        strokeWidth: 3, // optional
+      },
+      {
+        data: [8, 5, 6, 8, 4],
+        color: () => `rgba(243, 74, 47, 1)`, // optional
+        strokeWidth: 3, // optional
       },
     ],
-    legend: ["Rainy Days", "ok"], // optional
+    legend: ["Vay", "Thu", "Chi"], // optional
   }
+  const [dataFromChart, setDataFromChart] = useState({})
+  const screenWidth = Dimensions.get("window").width
+  const [index, setIndex] = useState(0)
+  const [routes] = useState([
+    { key: "first", title: "Vay" },
+    { key: "second", title: "Chi" },
+    { key: "third", title: "Thu" },
+  ])
+
   return (
     <View>
       <Text
@@ -42,13 +54,69 @@ const HomeScreen = () => {
       </Text>
       <LineChart
         data={data}
-        width={Dimensions.get("window").width}
+        width={screenWidth}
         height={256}
-        verticalLabelRotation={30}
+        verticalLabelRotation={0}
         chartConfig={chartConfig}
         bezier
+        withShadow={false}
+        withDots={false}
+        yAxisSuffix=" tr"
+        onDataPointClick={(data) => {
+          console.log(data)
+        }}
       />
+
+      <View
+        style={{
+          backgroundColor: "#212230",
+          height: "100%",
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontWeight: "bold",
+            fontSize: 16,
+            textAlign: "center",
+            paddingTop: 20,
+          }}
+        >
+          Thống kê trong tháng 7
+        </Text>
+        <TabView
+          navigationState={{ index, routes }}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          initialLayout={{ width: screenWidth }}
+          style={{ backgroundColor: "black" }}
+          renderTabBar={renderTabBar}
+        />
+      </View>
     </View>
   )
 }
-export default HomeScreen
+export default Statistic
+
+const renderScene = SceneMap({
+  first: () => <View style={{ flex: 1, backgroundColor: "#ff4081" }} />,
+  second: () => <View style={{ flex: 1, backgroundColor: "#673ab7" }} />,
+  third: () => <View style={{ flex: 1, backgroundColor: "violet" }} />,
+})
+
+const renderTabBar = (props: any) => (
+  <TabBar
+    {...props}
+    indicatorStyle={{ backgroundColor: "#3CD3AD" }}
+    style={{ backgroundColor: "#212230" }}
+    renderLabel={({ route, focused }) =>
+      focused ? (
+        <Text style={{ color: "#3CD3AD" }}>{route.title}</Text>
+      ) : (
+        <Text style={{ color: "gray" }}>{route.title}</Text>
+      )
+    }
+  />
+)
