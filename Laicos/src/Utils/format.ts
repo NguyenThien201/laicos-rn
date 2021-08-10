7c48f07ec417784ab4142a889786a2b8ba33fda8
Dang Khoa Nguyen <nguyendangkhoa200400@gmail.com>
2021-08-11 00:03:33 +0700

    Formatter

--- Laicos/src/Utils/format.ts
+++ Laicos/src/Utils/format.ts
@@ -1,36 +1,28 @@
-
-
 export const formatter = (number: number): string => {
-    let result: string = ""
-    let isSign: boolean = false
-   
-    let n = number
-    const re = new RegExp(',', 'g');
-   
-    if (number < 0)
-    {
-        n *= -1
-        isSign = true
-    }
-    else if(n<10)
-    {
-        return n+""
-    }
-    while (n > 0) {
-      
-        let unit = n % 10
-        result = unit + result
-        if (result.replace(re,"").length % 3 === 0) {
-            result = "," +result
-        }
-        n = Math.floor(n/10)
-    }   
-    if (result[0] === ",") result = result.substring(1)
-    if (isSign) result = "-" + result
-    return result
-
-}
+	let result: string = "";
+	let isSign: boolean = false;
+	let n = Math.floor(number);
+	const re = new RegExp(",", "g");
+	let decimal_part = Math.floor((number - n) * 100);
+	if (number < 0) {
+		n *= -1;
+		isSign = true;
+	} else if (n < 10) {
+		return n + "";
+	}
+	while (n > 0) {
+		let unit = n % 10;
+		result = unit + result;
+		if (result.replace(re, "").length % 3 === 0) {
+			result = "," + result;
+		}
+		n = Math.floor(n / 10);
+	}
+	if (result[0] === ",") result = result.substring(1);
+	if (isSign) result = "-" + result;
+	return result + "." + decimal_part;
+};
 
 export function toCommas(value: number): string {
-    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
